969a688b1d3963181ee8113009daf15f8583a04a
priyankasahoo01 <38129444+priyankasahoo01@users.noreply.github.com>
2019-07-30 09:08:08 +0530

    completed merge fn

--- Algorithm/src/com/sort/MergeSort.java
+++ Algorithm/src/com/sort/MergeSort.java
@@ -9,15 +9,44 @@ public class MergeSort implements Sort {
     }
 
     private void mergeSort(int[] ip, int l, int r) {
-        int mid = (l + r) / 2;
+        if( l >= r) {
+			return;
+		}
+        int mid = l+(r-l)/2;
         mergeSort(ip, l, mid);
         mergeSort(ip, mid + 1, r);
         merge(ip, l, r, mid);
 
     }
 
-    private void merge(int[] ip, int l, int r, int mid) {
-       for(int i )
-    }
+    private void merge(int[] arr, int l, int mid, int r) {
+		// TODO Auto-generated method stub
+		
+		int i = l, j = mid+1;
+		int end1 = mid, end2 = r; 
+		int[] op = new int[r-l+1];
+		int k = 0;
+		while(i <= end1 && j <= end2) {
+			if(arr[i] <= arr[j]) {
+				op[k++] = arr[i++];
+			}else {
+				op[k++] = arr[j++];
+			}
+		}
+		
+		while( i <= end1) {
+			op[k++] = arr[i++];
+		}
+		while( j <= end2){ 
+			op[k++] = arr[j++];
+		}
+		
+		//copy arr
+		k = 0;
+		for( int t = l ; t <= r; t++) {
+			arr[t] = op[k++];
+		}
+		
+	}
 
 }
